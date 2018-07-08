b23d98b1ac24f2fc5752aaf8e90f7938e60337e9
bestsuperweb <bestsuperweb@gmail.com>
2018-07-07 22:10:11 -0400

    ...

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -19,7 +19,7 @@ class Block {
 }
 
 const genesisBlock: Block = new Block(
-    0, '816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7', '', 1465154705, 'my genesis block!!'
+    0, '816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7', '', 1465154705, 'genesis block!!'
 );
 
 let blockchain: Block[] = [genesisBlock];
